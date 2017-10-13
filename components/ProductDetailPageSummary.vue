5c92e4b802ba344fb561f575be770cd0057dcf5c
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-10-13 08:56:41 +0200

    Added all products

--- components/ProductDetailPageSummary.vue
+++ components/ProductDetailPageSummary.vue
@@ -162,12 +162,11 @@ export default {
 
 .image {
   position: relative;
-  padding-bottom: 75%;
+  padding-bottom: 100%;
   width: 100%;
 }
 .image img {
   position: absolute;
-  bottom: 0;
   left: 0;
   right: 0;
   top: 0;
