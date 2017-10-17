ee699796e1682dc17f111bf3151f6db04e12cb27
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-10-17 12:36:20 +0200

    Products without description

--- components/ProductDetailPageSummaryDescription.vue
+++ components/ProductDetailPageSummaryDescription.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <p>{{product.shortDescription.slice(0,250)}}... <nuxt-link to="#description">Læs mere</nuxt-link></p>
-    <h2 class="has-small-top-margin">Nøglespecifikationer</h2>
-    <ul class="is-unstyled-list has-tiny-top-margin">
-      <li v-for="keySpec in product.keySpecifications">
-        <strong class="is-dimmed">{{keySpec.key}}:</strong> {{keySpec.value}}
-      </li>
-    </ul>
-    <p class="has-tiny-top-margin"><a href="#specifications">Se alle specifikationer</a></p>
+    <p v-if="product.shortDescription">{{product.shortDescription.slice(0,250)}}... <nuxt-link to="#description">Læs mere</nuxt-link></p>
+    <template v-if="product.keySpecifications && product.keySpecifications.length > 0">
+      <h2 class="has-small-top-margin">Nøglespecifikationer</h2>
+      <ul class="is-unstyled-list has-tiny-top-margin">
+        <li v-for="keySpec in product.keySpecifications">
+          <strong class="is-dimmed">{{keySpec.key}}:</strong> {{keySpec.value}}
+        </li>
+      </ul>
+      <p class="has-tiny-top-margin"><a href="#specifications">Se alle specifikationer</a></p>
+    </template>
   </div>
 </template>
 
