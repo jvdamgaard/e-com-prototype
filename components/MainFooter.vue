e54685b8540867b488c41489053a7795f403c43c
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-10-20 22:15:32 +0200

    Footer with css modules

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,99 +1,63 @@
 <template>
-  <footer class="MainFooter">
+  <footer :class="$style.container">
     <unique-selling-points />
     <footer-highlighted-content />
-    <grid row-gap="large" class="Links has-white-background">
-      <div class="is-12-col is-6-col-on-phablet is-3-col-on-tablet is-2-col-on-laptop">
-        <h3 class="has-small-bottom-margin">Bilka</h3>
-        <p><nuxt-link to="/" class="is-grey">Om os</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Kerneværdier</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Karriere</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Blog</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Engineering</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Sitemap</nuxt-link></p>
-      </div>
-      <div class="is-12-col is-6-col-on-phablet is-3-col-on-tablet is-2-col-on-laptop">
-        <h3 class="has-small-bottom-margin">Brug for hjælp?</h3>
-        <p><nuxt-link to="/" class="is-grey">Kundeservice</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Find din Bilka</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Sådan handler du på bilka.dk</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Levering</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Returnering</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Reklamation</nuxt-link></p>
-      </div>
-      <div class="is-12-col is-6-col-on-phablet is-3-col-on-tablet is-2-col-on-laptop">
-        <h3 class="has-small-bottom-margin">Inspiration og Tips</h3>
-        <p><nuxt-link to="/" class="is-grey">Shopping inspiration</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Shopping guides</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Set i TV</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Mærker på bilka.dk</nuxt-link></p>
-      </div>
-      <div class="is-12-col is-6-col-on-phablet is-3-col-on-tablet is-2-col-on-laptop">
-        <h3 class="has-small-bottom-margin">Tjen penge sammen med os</h3>
-        <p><nuxt-link to="/" class="is-grey">Online shopping</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Sælg på bilka.dk</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Annoncer på bilka.dk</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Medie forespørgsler</nuxt-link></p>
-        <p><nuxt-link to="/" class="is-grey">Bliv en affiliate</nuxt-link></p>
-      </div>
-      <div class="is-12-col is-4-col-on-laptop">
-        <h3 class="has-small-bottom-margin">Tilmeld nyhedsbrev</h3>
-        <div class="is-group">
-          <input type="text" placeholder="Din e-mail adresse" class="has-light-grey-background no-border">
+    <grid row-gap="large" :class="$style.links">
+      <grid-col
+        v-for="linkSection in linkSections"
+        phablet="6"
+        tablet="3"
+        laptop="2"
+      >
+        <h3 :class="$style.header">{{ linkSection.header }}</h3>
+        <p v-for="link in linkSection.links">
+          <nuxt-link to="/" :class="$style.link">{{ link.label }}</nuxt-link>
+        </p>
+      </grid-col>
+      <grid-col laptop="4">
+        <h3 :class="$style.header">Tilmeld nyhedsbrev</h3>
+        <div :class="$style.newsletterGroup">
+          <input type="text" placeholder="Din e-mail adresse" :class="$style.newsletterInput">
           <Btn type="grey" height="small">Tilmeld</Btn>
         </div>
-        <p class="is-small is-dimmed">
+        <p :class="$style.newsletterDescription">
           Tilmeld dig Bilkas nyhedsbrev og få ugens tilbudsavis samt gode tilbud, information om spændende kampagner og konkurrencer.<br>
           Hver måned trækkes en vinder af tre måneders forbrug af Nivea produkter til dame eller herre blandt alle modtagere af nyhedsmailen.
         </p>
-      </div>
+      </grid-col>
     </grid>
-    <grid row-gap="large" class="Links has-white-background">
-      <div class="is-12-col is-4-col-on-tablet">
-        <h3 class="has-small-bottom-margin">Betalingsmuligheder</h3>
+    <grid row-gap="large" :class="$style.links">
+      <grid-col
+        v-for="imageSection in imageSections"
+        tablet="4"
+      >
+        <h3 :class="$style.header">{{ imageSection.header }}</h3>
         <p>
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/dankort.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/master-card.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/mobilepay.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/paypal.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/visa-electron.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/visa.png'" />
+          <img
+            v-for="image in imageSection.images"
+            v-lazy="image.src"
+            />
         </p>
-      </div>
-      <div class="is-12-col is-4-col-on-tablet">
-        <h3 class="has-small-bottom-margin">Partnere</h3>
-        <p>
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/partners/iposen.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/partners/wupti.png'" />
-        </p>
-      </div>
-      <div class="is-12-col is-4-col-on-tablet">
-        <h3 class="has-small-bottom-margin">Følg os</h3>
-        <p>
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/facebook.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/instagram.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/twitter.png'" />
-          <img v-lazy="'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/youtube.png'" />
-        </p>
-      </div>
+      </grid-col>
     </grid>
-    <grid class="Links has-white-background is-small">
-      <div class="is-12-col is-6-col-on-tablet">
-        <p class="is-grey">Copyright Dansk Supermarked Group</p>
-      </div>
-      <div class="is-12-col is-6-col-on-tablet">
-        <p class="is-aligned-right">
-          <nuxt-link to="/" class="is-grey">Datapolitk</nuxt-link>
+    <grid :class="$style.bottomNav">
+      <grid-col tablet="6">
+        <p>Copyright Dansk Supermarked Group</p>
+      </grid-col>
+      <grid-col tablet="6">
+        <p :class="$style.bottomNavLinks">
+          <nuxt-link to="/">Datapolitk</nuxt-link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <nuxt-link to="/" class="is-grey">Handelsbetingelser</nuxt-link>
+          <nuxt-link to="/">Handelsbetingelser</nuxt-link>
         </p>
-      </div>
+      </grid-col>
     </grid>
   </footer>
 </template>
 
 <script>
 import Grid from './Grid.vue';
+import GridCol from './GridCol.vue';
 import Btn from './Btn.vue';
 import UniqueSellingPoints from './UniqueSellingPoints.vue';
 import FooterHighlightedContent from './FooterHighlightedContent.vue';
@@ -101,28 +65,124 @@ import FooterHighlightedContent from './FooterHighlightedContent.vue';
 export default {
   components: {
     Grid,
+    GridCol,
     UniqueSellingPoints,
     FooterHighlightedContent,
     Btn,
   },
+  data() {
+    return {
+      linkSections: [{
+        header: 'Bilka',
+        links: [
+          { label: 'Om os', link: '/' },
+          { label: 'Kerneværdier', link: '/' },
+          { label: 'Karriere', link: '/' },
+          { label: 'Blog', link: '/' },
+          { label: 'Engineering', link: '/' },
+          { label: 'Sitemap', link: '/' },
+        ],
+      }, {
+        header: 'Brug hjælp?',
+        links: [
+          { label: 'Kundeservice', link: '/' },
+          { label: 'Find din Bilka', link: '/' },
+          { label: 'Sådan handler du på bilka.dk', link: '/' },
+          { label: 'Levering', link: '/' },
+          { label: 'Returnering', link: '/' },
+          { label: 'Reklamation', link: '/' },
+        ],
+      }, {
+        header: 'Inspiration & Tips',
+        links: [
+          { label: 'Shopping inspiration', link: '/' },
+          { label: 'Shopping guides', link: '/' },
+          { label: 'Set i TV', link: '/' },
+          { label: 'Mærker på bilka.dk', link: '/' },
+        ],
+      }, {
+        header: 'Tjen sammen med os',
+        links: [
+          { label: 'Online shopping', link: '/' },
+          { label: 'Sælg på bilka.dk', link: '/' },
+          { label: 'Annoncer på bilka.dk', link: '/' },
+          { label: 'Medie forespørgsler', link: '/' },
+          { label: 'Bliv en affiliate', link: '/' },
+        ],
+      }],
+      imageSections: [{
+        header: 'Betalingsmuligheder',
+        images: [
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/dankort.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/master-card.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/mobilepay.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/paypal.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/visa-electron.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/payment-methods/visa.png' },
+        ],
+      }, {
+        header: 'Partnere',
+        images: [
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/partners/iposen.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/partners/wupti.png' },
+        ],
+      }, {
+        header: 'Følg os',
+        images: [
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/facebook.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/instagram.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/twitter.png' },
+          { src: 'https://jvdamgaard.github.io/e-com-prototype/icons/social-media/youtube.png' },
+        ],
+      }],
+    };
+  },
 };
 </script>
 
-<style>
-  @import '../assets/css/variables.css';
+<style module>
+@import '../assets/css/variables.css';
 
-  .MainFooter > * { margin-bottom: 1px; }
-  .MainFooter > *:not(:first-child) { margin-top: 1px; }
+.container > * { margin-bottom: 1px; }
+.container > *:not(:first-child) { margin-top: 1px; }
 
-  .Links {
-    padding-top: 3.5rem;
-    padding-bottom: 3.5rem;
-  }
+.header { margin-bottom: 1rem; }
 
-  .Links img {
-    display: inline-block;
-    height: 1.5rem;
-    margin-right: 1rem;
-  }
+.links {
+  padding-top: 3.5rem;
+  padding-bottom: 3.5rem;
+  background-color: var(--color-white);
+}
 
+.links img {
+  display: inline-block;
+  height: 1.5rem;
+  margin-right: 1rem;
+}
+
+.link {
+  composes: small from './styles.css';
+  color: var(--color-grey-darker);
+}
+
+.newsletterGroup {
+  composes: group from './styles.css';
+}
+.newsletterInput {
+  background-color: var(--color-grey-lighter);
+  border: 0;
+}
+.newsletterDescription {
+  composes: small from './styles.css';
+  color: var(--color-grey-dark);
+}
+
+.bottomNav {
+  composes: links;
+  composes: small from './styles.css';
+  background-color: var(--color-white);
+  color: var(--color-grey-darker);
+}
+.bottomNav a { color: var(--color-grey-darker); }
+.bottomNavLinks { text-align: right; }
 </style>
