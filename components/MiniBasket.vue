f983532fe46dcf65bdf2ce4bf62768a006f26739
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-10-23 21:21:40 +0200

    MiniBasket as css modules

--- components/MiniBasket.vue
+++ components/MiniBasket.vue
@@ -1,51 +1,60 @@
 <template>
   <div
-    class="MiniBasket"
-    :class="{ 'MiniBasket--active': state.miniBasketActive }"
+    :class="[
+      $style.container,
+      { [$style.active]: state.miniBasketActive }
+    ]"
     @click.self="close"
   >
-    <grid class="has-no-top-margin has-no-bottom-margin" @click.native.self="close">
-      <div
-        class="is-hidden-on-mobile is-visible-on-tablet is-5-col is-7-col-on-laptop is-9-col-on-desktop"
+    <grid :class="$style.grid" @click.native.self="close">
+      <grid-col
+        mobile="0"
+        tablet="5"
+        laptop="7"
+        desktop="9"
         @click="close"
       />
-      <div
-        class="is-12-col is-7-col-on-tablet is-5-col-on-laptop is-3-col-on-desktop has-shadow"
+      <grid-col
+        tablet="7"
+        laptop="5"
+        desktop="3"
+        class="shadow"
         @mouseover="enter"
         @mouseleave="leave"
       >
-        <div class="has-light-grey-background">
-          <div class="MiniBasket__items">
+        <div :class="$style.background">
+          <div :class="$style.items">
             <mini-basket-item
               v-for="item in user.basket.items"
               :key="item.product.id"
               v-bind="item"
             />
           </div>
-          <div class="MiniBasket__total is-h3 clearfix">
+          <div :class="$style.total">
             {{itemsInBasket}} produkter total
-            <div class="MiniBasket__total-price">
-              <span v-if="totalBeforePriceInBasket > totalPriceInBasket" class="MiniBasket__before-price is-small">
+            <div :class="$style.totalPrice">
+              <span v-if="totalBeforePriceInBasket > totalPriceInBasket" :class="$style.beforePrice">
                 {{numberWithDots(totalBeforePriceInBasket)}} kr
               </span>
-              <span class="is-h3 has-tiny-left-margin" :class="{
-                'is-red': totalBeforePriceInBasket > totalPriceInBasket,
-              }">{{numberWithDots(totalPriceInBasket)}} kr</span>
+              <span class="" :class="[
+                $style.price,
+                { [$style.red]: totalBeforePriceInBasket > totalPriceInBasket }
+              ]">{{numberWithDots(totalPriceInBasket)}} kr</span>
             </div>
           </div>
-          <grid inner class="has-horizontal-padding">
-            <div class="is-6-col">
+          <grid inner :class="$style.bottom">
+            <grid-col mobile="6">
               <btn type="grey">Se kurven</btn>
-            </div>
-            <div class="is-6-col">
+            </grid-col>
+            <grid-col mobile="6">
               <btn type="buy" shadow>Til kassen</btn>
-            </div>
+            </grid-col>
           </grid>
-          <div class="MiniBasket__selling-point is-aligned-center has-large-top-padding has-large-bottom-padding">
+          <div :class="$style.sellingPoint">
             Gratis fragt ved køb over 1.000 kr
           </div>
         </div>
-      </div>
+      </grid-col>
     </grid>
   </div>
 </template>
@@ -55,12 +64,14 @@ import { mapState, mapActions } from 'vuex'; // eslint-disable-line
 import debounce from 'lodash/debounce';
 import { numberWithDots } from '../utils';
 import Grid from './Grid.vue';
+import GridCol from './GridCol.vue';
 import Btn from './Btn.vue';
 import MiniBasketItem from './MiniBasketItem.vue';
 
 export default {
   components: {
     Grid,
+    GridCol,
     Btn,
     MiniBasketItem,
   },
@@ -105,35 +116,53 @@ export default {
 };
 </script>
 
-<style>
+<style module>
 @import '../assets/css/variables.css';
 
-.MiniBasket {
+.container {
   background-color: var(--color-pop-over-background);
   min-height: calc(100vh - 3.5rem);
   display: none;
 }
-.MiniBasket--active {
-  display: block;
+.active { display: block; }
+
+.grid {
+  margin-top: 0;
+  margin-bottom: 0;
 }
 
-.MiniBasket__items {
+.background { background-color: var(--color-grey-lighter); }
+
+.items {
   max-height: calc(100vh - 20rem);
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
 }
 
-.MiniBasket__total {
+.total {
+  composes: h3 from global;
+  composes: clearfix from global;
   padding: 2rem 1rem;
 }
-.MiniBasket__before-price {
+.totalPrice { float: right; }
+.beforePrice {
+  composes: small from global;
   color: var(--color-grey);
   text-decoration: line-through;
 }
-.MiniBasket__total-price {
-  float: right;
+.price {
+  composes: h3 from global;
+  margin-left: 0.5rem;
+}
+.red { color: var(--color-red); }
+
+.bottom {
+  padding-left: 1rem;
+  padding-right: 1rem;
 }
 
-@media (min-width: 48rem) {
+.sellingPoint {
+  text-align: center;
+  padding: 2rem 0;
 }
 </style>
