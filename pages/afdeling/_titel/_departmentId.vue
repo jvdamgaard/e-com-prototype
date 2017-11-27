1754d7a4a4340319bab2de96f56b836d1a23d013
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-11-27 15:39:22 +0100

    Posibility to have departments

---
+++ pages/afdeling/_titel/_departmentId.vue
@@ -0,0 +1,25 @@
+<template>
+  <sections :sections="sections" />
+</template>
+
+<script>
+import Sections from '../../../components/Sections.vue';
+import { getPageSections } from '../../../utils/page';
+
+export default {
+  components: {
+    Sections,
+  },
+  async asyncData({ params }) {
+    const sections = await getPageSections(params.departmentId);
+    return { sections };
+  },
+  mounted() {
+    const initPos = (window.pageYOffset > 33) ? 33 : 0;
+    window.scrollTo(0, initPos);
+  },
+};
+</script>
+
+<style>
+</style>
