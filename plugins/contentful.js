80aa6c509f79493dac7218325f4d5f58caa7ef58
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-12-20 20:25:28 +0100

    Use netlify proxy

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -8,4 +8,6 @@ export const deliveryClient = contentful
   .createClient({
     space: process.env.CTF_SPACE_ID,
     accessToken: process.env.CTF_CD_ACCESS_TOKEN,
+    host: 'mrkt.netlify.com',
+    basePath: 'api/contentful',
   });
