2b2c007e5cfcbfc138b6537a80fa0e2d402d15c7
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-12-21 08:43:25 +0100

    Use employme.at proxy

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -2,12 +2,16 @@ import * as contentfulManagement from 'contentful-management';
 import * as contentful from 'contentful';
 
 export const managementClient = contentfulManagement
-  .createClient({ accessToken: process.env.CTF_CM_ACCESS_TOKEN });
+  .createClient({
+    accessToken: process.env.CTF_CM_ACCESS_TOKEN,
+    host: 'employme.at',
+    basePath: 'api/contentful',
+  });
 
 export const deliveryClient = contentful
   .createClient({
     space: process.env.CTF_SPACE_ID,
     accessToken: process.env.CTF_CD_ACCESS_TOKEN,
-    host: 'mrkt.netlify.com',
+    host: 'employme.at',
     basePath: 'api/contentful',
   });
