8d33277b1e609089cb9072f681dc9d99b8c7a7e5
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-12-26 20:21:01 +0100

    Try again

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -2,14 +2,14 @@ import * as contentfulManagement from 'contentful-management';
 import * as contentful from 'contentful';
 
 export const managementClient = contentfulManagement.createClient({
-  accessToken: process.env.CTF_CM_ACCESS_TOKEN,
+  accessToken: process.env.CTF_CM_ACCESS_TOKEN || 'netlify',
   host: process.env.DEPLOY_PRIME_URL,
   basePath: process.env.DEPLOY_PRIME_URL ? 'api/contentful-management' : undefined,
 });
 
 export const deliveryClient = contentful.createClient({
   space: 'n8ckv2qtuzei',
-  accessToken: process.env.CTF_CD_ACCESS_TOKEN,
+  accessToken: process.env.CTF_CD_ACCESS_TOKEN || 'netlify',
   host: process.env.DEPLOY_PRIME_URL,
   basePath: process.env.DEPLOY_PRIME_URL ? 'api/contentful-delivery' : undefined,
 });
