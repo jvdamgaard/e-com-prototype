7a5d820dd48d0091aa591be5c63d4849501bf3fa
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-12-26 20:17:51 +0100

    No env vars

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,13 +1,15 @@
 import * as contentfulManagement from 'contentful-management';
 import * as contentful from 'contentful';
 
-export const managementClient = contentfulManagement
-  .createClient({ accessToken: process.env.CTF_CM_ACCESS_TOKEN });
+export const managementClient = contentfulManagement.createClient({
+  accessToken: process.env.CTF_CM_ACCESS_TOKEN,
+  host: process.env.DEPLOY_PRIME_URL,
+  basePath: process.env.DEPLOY_PRIME_URL ? 'api/contentful-management' : undefined,
+});
 
-export const deliveryClient = contentful
-  .createClient({
-    space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_CD_ACCESS_TOKEN,
-    host: 'employme.at',
-    basePath: 'api/contentful',
-  });
+export const deliveryClient = contentful.createClient({
+  space: 'n8ckv2qtuzei',
+  accessToken: process.env.CTF_CD_ACCESS_TOKEN,
+  host: process.env.DEPLOY_PRIME_URL,
+  basePath: process.env.DEPLOY_PRIME_URL ? 'api/contentful-delivery' : undefined,
+});
