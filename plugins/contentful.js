2a656b1208a5250c39589e61349c4ef94281e268
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-12-27 23:05:02 +0100

    A trigger

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,8 +3,8 @@ const contentful = require('contentful');
 
 module.exports.managementClient = contentfulManagement.createClient({
   accessToken: 'CFPAT-802737470517e9206951077be900c1fa1dcac09d26c9d282a5ecc3d78047eb78',
-  host: 'employme.at',
-  basePath: 'api/contentful-management',
+  // host: 'employme.at',
+  // basePath: 'api/contentful-management',
 });
 
 module.exports.deliveryClient = contentful.createClient({
